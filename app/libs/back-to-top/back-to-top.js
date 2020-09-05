ae71ce1095ce79d1f684c4f6dd2a8c40572e9a5e
Isabela Fitero <isabelafitero@gmail.com>
2020-09-06 01:55:34 +0300

    remove backToTop css file

--- app/libs/back-to-top/back-to-top.js
+++ app/libs/back-to-top/back-to-top.js
@@ -3,8 +3,21 @@ const backToTop = angular.module('backToTop', []);
 backToTop.directive('backToTop', function () {
   return {
     restrict: 'A',
-    link: function (scope, element, attrs) {
-      element.addClass('back-to-top');
+    controller: [function BackToTopController() {
+      var self = this;
+
+      self.css = {
+        'z-index': '2',
+        'display': 'none',
+        'bottom': '0',
+        'position': 'fixed',
+        'margin-left': '1em',
+        'margin-bottom': '1em',
+        'transition': 'transform .5s'
+      };
+    }],
+    link: function (scope, element, attrs, controller) { 
+      element.css(controller.css);
 
       $(element).on('click', () => {
 				$('html, body').animate({ scrollTop: 0 }, 'fast');
