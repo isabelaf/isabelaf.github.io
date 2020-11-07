90fc553d6c864c37275e0e8230533cd3a413601d
Isabela Fitero <isabelafitero@gmail.com>
2020-11-07 12:10:03 +0200

    finish portofolio page

--- src/app/empty-page/empty-page.component.ts
+++ src/app/empty-page/empty-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { ActivatedRoute, Router } from '@angular/router';
+
 import { AppConstants } from '../app.constants';
 
 @Component({
--- src/app/portofolio/portofolio.component.ts
+++ src/app/portofolio/portofolio.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-portofolio',
-  templateUrl: './portofolio.component.html'
+  templateUrl: './portofolio.component.html',
+  styleUrls: ['./portofolio.component.scss']
 })
 export class PortofolioComponent {
   src: string = '../../assets/data/portofolio.pdf';
