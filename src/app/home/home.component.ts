734b7df595b30c3a48c74dcaf11b76669ccc69e4
Isabela Fitero <isabelafitero@gmail.com>
2020-11-08 00:18:23 +0200

    move home scss to component folder

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,7 +5,8 @@ import { Details } from '../models/details.model';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
   details: Details = new Details();
