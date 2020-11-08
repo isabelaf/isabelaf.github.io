00f041309e878293e3036871d4c362f57e5df86e
Isabela Fitero <isabelafitero@gmail.com>
2020-11-08 18:15:06 +0200

    fix styles

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,8 +5,7 @@ import { Details } from '../models/details.model';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
   details: Details = new Details();
--- src/app/portofolio/portofolio.component.ts
+++ src/app/portofolio/portofolio.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-portofolio',
-  templateUrl: './portofolio.component.html',
-  styleUrls: ['./portofolio.component.scss']
+  templateUrl: './portofolio.component.html'
 })
 export class PortofolioComponent {
   src: string = '../../assets/data/portofolio.pdf';
